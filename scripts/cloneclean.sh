eb39a72444acf34a2f08f83278efa6af9cc4daea
kakyoinnlerolero <55839379+kakyoinnlerolero@users.noreply.github.com>
2019-09-28 13:13:01 +0530

    comment delete

--- scripts/cloneclean.sh
+++ scripts/cloneclean.sh
@@ -17,7 +17,7 @@ cloneclean() {
     # Remove empty directories
     find "$hdpath/move" -mindepth 2 -type d -empty -delete
     #DO NOT decrease DEPTH on this, leave it at 3. Leave this alone!
-    find "$hdpath/downloads" -mindepth 3 -type d \( ! -name syncthings ! -name .stfolder \) -empty -delete
+    #find "$hdpath/downloads" -mindepth 3 -type d \( ! -name syncthings ! -name .stfolder \) -empty -delete
 
     # Prevents category folders underneath the downloaders from being deleted, while removing empties from the import process.
     # This was done to address some apps having an issue if the category underneath the downloader is missing.
